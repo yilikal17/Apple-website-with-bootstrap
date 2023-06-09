983acb02793727a2a6dd6433bd0139ef471c03b7
yilikal17 <yilikalbelayneh17@gmail.com>
2023-06-09 09:40:33 +0300

    first commit

---
+++ js/toogle.js
@@ -0,0 +1,9 @@
+$(document).on("click", ".footer-links-wrapper h3", function () {
+    if ($(window).width() < 768) {
+    if ($(this).next("ul").fadeToggle()) $(this).toggleClass("expanded");
+    }
+    $(window).on("resize", function () {
+    window.location.reload();
+    });
+    });
+
